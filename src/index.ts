59f9c02f70d038053d7929a49b395a487900c55f
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-01-14 12:19:24 +0100

    express initialized

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port 3000 !');
+});
+
